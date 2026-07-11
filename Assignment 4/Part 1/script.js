6363073bed88af5e02e3911d57676e35572dae57
Harman591101 <harmanpreet.singh18@dcmail.ca>
2026-07-11 08:57:52 -0400

    ready to deploy

--- Assignment 4/Part 1/script.js
+++ Assignment 4/Part 1/script.js
@@ -9,7 +9,7 @@ function randomValueFromArray(array) {
   return array[randomIndex];
 }
 
-// Story ingredients
+// Story ingredients.
 const characters = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
 const places = ["the soup kitchen", "Disneyland", "the White House"];
 const events = [
